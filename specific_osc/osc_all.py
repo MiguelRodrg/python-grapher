7f6b87bbd782fbc623b1c4d47c83e36ddf203c7e
MiguelRodrg <miarodriguezgo@unal.edu.co>
2025-11-02 21:55:43 -0500

    others

--- specific_osc/osc_all.py
+++ specific_osc/osc_all.py
@@ -1,8 +1,15 @@
 from .convertions import to_second
 
-def estandarize(table,new_table):
-    interval = to_second(table.attrs['Time interval'][0])
-    new_table['index'] = new_table['index']*interval
-    new_table.rename(columns={"index":"Tiempo"},inplace=True)
-    return new_table
+# def estandarize(table):
+#     print(f"Atributos: {table.attrs}")
+#     interval = to_second(table.attrs['Time interval'][0])
+#     print(f"Intervalo de tiempo en segundos: {interval}")
+#     print(new_tale['Tiempo'])
+#     table['Tiempo'] = table['Tiempo']*interval[0]
+#     print(f"Primeros 5 valores de tiempo estandarizados: {table['Tiempo'].head()}")
+#     print("Estandarización completa.")
 
+def time(time_interval,list):
+    units = to_second(time_interval)
+    list = [x*units for x in list]
+    return list
